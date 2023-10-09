aa7f548a1efce9020b45614bda2b5d47bf54ec40
YarodAB <yarod.aliaga@gmail.com>
2023-10-09 17:58:07 -0300

    Se corrigen los errores de la case Libro, que colabora directamente con la clase autor

--- libreriaAteneo/src/com/libreriaAteneo/Libro.java
+++ libreriaAteneo/src/com/libreriaAteneo/Libro.java
@@ -9,20 +9,70 @@ package com.libreriaAteneo;
  * @author Yarod
  */
 public class Libro {
+    
+    //Atributos
     private String titulo;
-    private String Autor;
+    private Autor autor;
     private int precio;
     private String isbn;
 
+    
+    //Constructor Vacio
     public Libro() {
     }
 
-    public Libro(String titulo, String Autor, int precio, String isbn) {
+    
+    //Constructor con info
+    public Libro(String titulo, Autor autor, int precio, String isbn) {
         this.titulo = titulo;
-        this.Autor = Autor;
+        this.autor = autor;
         this.precio = precio;
         this.isbn = isbn;
     }
     
     
+    //Getters y Setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    
+    //Metodos customer
+    public void imprimirInfo(){
+        System.out.println("Titulo: " +  this.titulo + "\n Autor: " + this.autor + "\n Precio: " +this.precio + "\n ISBN: " + this.isbn);
+    }
+    
+    public double calcularIva(){
+       double IVA = 0.19;
+       
+       return this.precio * IVA;
+    
+    }
 }
