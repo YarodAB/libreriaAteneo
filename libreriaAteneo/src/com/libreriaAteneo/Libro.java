9b0d3fee076353a852bd0c74f8a82b6aaaca2393
YarodAB <yarod.aliaga@gmail.com>
2023-10-09 17:04:48 -0300

    Se incorpora y crea la clase Libro con sus atributos y constructores

---
+++ libreriaAteneo/src/com/libreriaAteneo/Libro.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.libreriaAteneo;
+
+/**
+ *
+ * @author Yarod
+ */
+public class Libro {
+    private String titulo;
+    private String Autor;
+    private int precio;
+    private String isbn;
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String Autor, int precio, String isbn) {
+        this.titulo = titulo;
+        this.Autor = Autor;
+        this.precio = precio;
+        this.isbn = isbn;
+    }
+    
+    
+}
