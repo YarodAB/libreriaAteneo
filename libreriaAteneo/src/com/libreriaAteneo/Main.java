07b67ab7dc0e7e0cc83897d63cc376a8411dfe44
YarodAB <yarod.aliaga@gmail.com>
2023-10-10 10:55:12 -0300

    Se agrega el main con todas las peticiones del usuario y negocio

--- libreriaAteneo/src/com/libreriaAteneo/Main.java
+++ libreriaAteneo/src/com/libreriaAteneo/Main.java
@@ -4,12 +4,71 @@
  */
 package com.libreriaAteneo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Yarod
  */
 public class Main {
     public static void main(String[] args) {
- 
+        Catalogo cat = new Catalogo();
+        Scanner scan = new Scanner(System.in);
+        
+        //A este mismo ejercicio podria agregar que cada libro de cada autor se vaya a una lista de libros del autor
+        System.out.println("1) AÑADIR LIBRO");
+        System.out.println("2) MOSTRAR CATALOGO");
+        System.out.println("3) BUSCAR LIBRO POR NOMBRE AUTOR");
+        System.out.println("4) ELIMINAR LIBRO POR ISBN");
+        System.out.println("5) SALIR");
+        System.out.println("");
+        System.out.println("SELECCIONA UNA OPCION");
+        
+        int opcion = scan.nextInt();
+        scan.nextLine();
+                
+        switch (opcion) {
+            case 1:
+                System.out.println("Ingrese el nombre del autor: ");
+                String nombre = scan.nextLine();
+                System.out.println("Ingrese el apellido del autor: ");
+                String apellido= scan.nextLine();
+                System.out.println("Ingrese la nacionalidad del autor: ");
+                String nacionalidad = scan.nextLine();
+                System.out.println("Ingrese el titulo del libro: ");
+                String titulo = scan.nextLine();
+                System.out.println("Ingrese el precio del libro: ");
+                int precio = scan.nextInt();
+                System.out.println("Ingrese el ISBN del libro: ");
+                String isbn = scan.nextLine();
+                scan.nextLine();
+                
+                
+                Autor autor = new Autor(nombre, apellido, nacionalidad);
+                Libro libro = new Libro(titulo, autor, precio, isbn);
+                
+                cat.agregaLibro(libro);
+                break;
+            case 2:
+                //Mostrar catalogo
+                cat.mostrarCatalogo();
+                break;
+            case 3:
+                //Buscar libro por nombre
+                System.out.println("Ingrese el nombre del autor a buscar: ");
+                String nombreAutor = scan.nextLine();
+                cat.buscarLibroNombre(nombreAutor);
+                break;
+            case 4:
+                System.out.println("Ingrese el ISBN del libro a eliminar: ");
+                String isbnEliminar = scan.nextLine();
+                cat.eliminarLibroIsbn(isbnEliminar);
+            case 5:
+                System.out.println("Adios!");
+                return;
+            default:
+                throw new AssertionError();
+        }
+
     }
 }
